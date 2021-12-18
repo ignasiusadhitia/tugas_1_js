56b2536174cd0cf2c20c8fc65e17ba83bcd8969f
Ignasius Yuda Adhitia <ignasius.yuda.adhitia@gmail.com>
2021-12-18 10:57:46 +0700

    Pengumpulan Tugas 1 JS - NIOMIC

--- tugas1.js
+++ tugas1.js
@@ -1,7 +1,6 @@
 var firstName = "Ignasius Yuda";
 let lastName = "Adhitia";
 const age = 34;
-
 const message1 = "Hello, my name is ";
 const message2 = ".\nI'm ";
 const message3 = " years old and I love to code.";
